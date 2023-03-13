b25a24325320e7dd299d1482bb87d07e69a3e602
alessiaBer <alexdeyn@gmail.com>
2023-03-13 15:16:22 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/* Chiedi all’utente la sua email, 
+controlla che sia nella lista di chi può accedere, 
+stampa un messaggio appropriato sull’esito del controllo */
+
+//creo un array contentente gli inidirizzi mail che possono accedere
+
+//chiedo all'user la sua mail e la assegno ad una variabile
+
+//verifico che la mail sia presente nell'array
+
+//stampo un messaggio appropriato sull'esito del controllo
